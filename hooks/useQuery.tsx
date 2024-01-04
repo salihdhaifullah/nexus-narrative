189b2566fc4a3ace2d5c3442d2b94b8b77fde01f
salehWeb <salehwebdev2004@gmail.com>
2024-01-04 22:03:45 +0200

    miggrated to remix its great

--- hooks/useQuery.tsx
+++
@@ -1,9 +0,0 @@
-import { useSearchParams } from "next/navigation";
-import { useMemo } from "react";
-
-const useQuery = () => {
-    const search = useSearchParams();
-    return useMemo(() => new URLSearchParams(search), [search]);
-}
-
-export default useQuery;
